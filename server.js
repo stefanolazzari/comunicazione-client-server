07254946e06d028f507d514322d0c931d48838a9
stefanolazzari <stefanolazzari79@libero.it>
2025-11-05 11:04:09 +0000

    app.get esempi

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("il server sta partendo...");
+const express = require('express');
+const app = express();
+const porta = 3000;
+
+//richieste get su http://localhost:3000/
+app.get("/", (req, res) => {
+  res.send("ok");
+})
+
+//richieste get su http://localhost:3000/saluto
+app.get("/saluto", (req, res) => {
+    res.send("Ciao");
+})
+
+app.listen(porta,function(){
+    console.log("server in ascolto su porta: " + porta);
+});
