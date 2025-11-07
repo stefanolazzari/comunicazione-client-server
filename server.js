19af9fe91785a79d8cdaba4c9ed51791c6232e73
stefanolazzari <stefanolazzari79@libero.it>
2025-11-07 09:07:13 +0000

    richieste get

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ const express = require('express');
 const app = express();
 const porta = 3000;
 
+
+let funghi = {
+  porcino: 4,
+  muscaria: 0,
+  chiodino: 3,
+  amanitaphalloides: -5
+}
+
+let autori = {
+  Adele: 3,
+  Toto:5,
+  frankzappa:10
+}
+
+
+
 //richieste get su http://localhost:3000/
 app.get("/", (req, res) => {
   res.send("ok");
@@ -12,6 +28,46 @@ app.get("/saluto", (req, res) => {
     res.send("Ciao");
 })
 
+app.get("/elencofunghi",(req, res) => {
+    res.send(funghi);
+})
+
+
+
+//richieste get su http://localhost:3000/prof
+app.get("/prof", (req, res) => {
+    res.send("Ciao dal prof, a me piace la polenta col baccalà");
+})
+
+
+
+
+
+
+app.get("/rayan", function(richiesta,risposta){
+    risposta.send("tutto ok da Rayan, a me piace la trap");
+    console.log(risposta);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(porta,function(){
     console.log("server in ascolto su porta: " + porta);
 });
+
